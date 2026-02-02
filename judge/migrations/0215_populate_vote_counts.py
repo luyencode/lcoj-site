ce70006949e8a46e3eed1fb7a3a9c0ec4112503f
Hieu Nguyen Van <hieunv.dev@gmail.com>
2026-02-02 19:20:20 +0700

    Revert "Feature/unified view"

--- judge/migrations/0215_populate_vote_counts.py
+++ judge/migrations/0215_populate_vote_counts.py
@@ -1,7 +1,6 @@
 from django.db import migrations
-from django.db.models import Count, IntegerField, OuterRef, Subquery
+from django.db.models import Count, OuterRef, Subquery, IntegerField
 from django.db.models.functions import Coalesce
-
 
 def populate_votes(apps, schema_editor):
     Comment = apps.get_model('judge', 'Comment')
@@ -10,25 +9,24 @@ def populate_votes(apps, schema_editor):
     # Subquery to count upvotes (score = 1)
     upvotes_qs = CommentVote.objects.filter(
         comment=OuterRef('pk'),
-        score=1,
+        score=1
     ).values('comment').annotate(
-        count=Count('id'),
+        count=Count('id')
     ).values('count')
-
+    
     # Subquery to count downvotes (score = -1)
     downvotes_qs = CommentVote.objects.filter(
         comment=OuterRef('pk'),
-        score=-1,
+        score=-1
     ).values('comment').annotate(
-        count=Count('id'),
+        count=Count('id')
     ).values('count')
 
     # Update all comments efficiently
     Comment.objects.update(
         upvotes=Coalesce(Subquery(upvotes_qs, output_field=IntegerField()), 0),
-        downvotes=Coalesce(Subquery(downvotes_qs, output_field=IntegerField()), 0),
+        downvotes=Coalesce(Subquery(downvotes_qs, output_field=IntegerField()), 0)
     )
-
 
 class Migration(migrations.Migration):
 
